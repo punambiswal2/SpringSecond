58a7329556051acb2f8067abf3967f7988c9a50e
punambiswal <punambiswal2@gmail.com>
2022-10-10 11:16:15 -0400

    Initial commit

---
+++ src/main/java/com/example/SpringSecond/SpringSecondApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecondApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecondApplication.class, args);
+	}
+
+}
